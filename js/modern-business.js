124db818e7ccf33558f4341f6d582c5c1265a8e7
Gary Yau <gyau88@gmail.com>
2014-01-17 10:50:33 -0800

    Added Data Height Match JS
    Added data column height match functionality using JavaScript, as well
    as file creation of Pet Sitting page.

--- js/modern-business.js
+++ js/modern-business.js
@@ -12,3 +12,33 @@ $('.tooltip-social').tooltip({
 $('.tooltip-footer').tooltip({
   selector: "img[data-toggle=tooltip]"
 })
+
+
+// Height Match for Service Price columns
+var dataHeightControl = function() {
+	if($(window).width() > 768) {
+		$("[data-match-height]").each(function() {
+			
+			$(this).find('.filler').css('min-height', 0);
+			
+			var parentRow = $(this),
+					childrenCols = $(this).find("[data-height-watch]"),
+					childHeights = childrenCols.map(function(){ return $(this).height(); }).get(),
+					tallestChild = Math.max.apply(Math, childHeights);
+		
+			
+			childrenCols.each(function() {
+				var fillerDiv = $(this).find('.filler');
+				fillerDiv.css('min-height', tallestChild - $(this).height());
+			});
+		});
+	};
+}
+
+$(window).load(function() {
+	dataHeightControl();
+});
+
+$(window).resize(function() {
+	dataHeightControl();
+});
